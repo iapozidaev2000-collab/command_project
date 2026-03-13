abb3c525d30ec747aa31f174cd2d678865801cb9
iapozidaev2000-collab <iapozidaev2000@gmail.com>
2026-03-13 20:59:35 +0300

    Добавление файла FileInputModeManualTest.java

--- src/test/java/ru/commandproject/manual/FileInputModeManualTest.java
+++ src/test/java/ru/commandproject/manual/FileInputModeManualTest.java
@@ -5,8 +5,6 @@ import ru.commandproject.input.ManualInputMode;
 import ru.commandproject.output.AppendFileWriter;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 public final class FileInputModeManualTest {
 
@@ -14,14 +12,14 @@ public final class FileInputModeManualTest {
         String testFilePath = "append.txt";
         AppendFileWriter writer = new AppendFileWriter(testFilePath);
 
+        // Теперь работаем через интерфейсный метод read
         ManualInputMode inputMode = new ManualInputMode();
-
-        BookCollection<Object> testCollection = createTestCollection();
 
         System.out.println("=== Запуск ручного теста: Чтение -> Коллекция -> Файл ===");
 
-        inputMode.startCollectionInput(testCollection);
-
+        // Вызываем метод read(count), который теперь является основным
+        // Укажите количество элементов, которые хотите ввести (например, 3)
+        BookCollection<Object> testCollection = inputMode.read(3);
 
         System.out.println("\n--- Сохранение накопленных данных в файл ---");
         writer.appendCollection("Результат ручного ввода", testCollection);
@@ -31,23 +29,6 @@ public final class FileInputModeManualTest {
 
         System.out.println("\nТест завершен.");
         System.out.println("Путь к файлу: " + new File(testFilePath).getAbsolutePath());
-        System.out.println("Проверьте, что в файле появились записи с заголовками 'Результат ручного ввода' и 'Отсортированная коллекция'.");
-    }
-
-
-    private static BookCollection<Object> createTestCollection() {
-        return new BookCollection<Object>() {
-            private final ArrayList<Object> storage = new ArrayList<>();
-
-            @Override
-            public void add(Object element) {
-                storage.add(element);
-            }
-
-            @Override
-            public Iterator<Object> iterator() {
-                return storage.iterator();
-            }
-        };
+        System.out.println("Проверьте файл на наличие записей.");
     }
 }
