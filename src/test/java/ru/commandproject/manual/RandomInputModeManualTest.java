c2728f7ebaeb364edfa3f870ea4548c60869aa35
iapozidaev2000-collab <iapozidaev2000@gmail.com>
2026-03-14 12:51:44 +0300

    Update RandomInputModeManualTest.java

--- src/test/java/ru/commandproject/manual/RandomInputModeManualTest.java
+++ src/test/java/ru/commandproject/manual/RandomInputModeManualTest.java
@@ -1,12 +1,12 @@
 package ru.commandproject.manual;
 
 import ru.commandproject.collection.BookCollection;
+import ru.commandproject.input.RandomInputMode; // Импортируем созданный класс
 import ru.commandproject.output.AppendFileWriter;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Random;
 
 public final class RandomInputModeManualTest {
 
@@ -14,33 +14,19 @@ public final class RandomInputModeManualTest {
         String testFilePath = "append.txt";
         AppendFileWriter writer = new AppendFileWriter(testFilePath);
 
+        RandomInputMode inputMode = new RandomInputMode();
         BookCollection<Object> testCollection = createTestCollection();
 
-        System.out.println("=== Запуск автоматического теста: Случайная генерация -> Коллекция -> Файл ===");
+        System.out.println("=== Запуск теста: RandomInputMode -> Коллекция -> Файл ===");
 
-        // Генерация случайных данных
-        fillCollectionWithRandomData(testCollection, 5);
-        System.out.println("Коллекция заполнена случайными данными (5 элементов).");
+        inputMode.startCollectionInput(testCollection);
 
         System.out.println("\n--- Сохранение случайных данных в файл ---");
         writer.appendCollection("Результат случайной генерации", testCollection);
 
-        System.out.println("\n--- Тест: Запись с заголовком по умолчанию ---");
-        writer.appendCollection(testCollection);
-
         System.out.println("\nТест завершен.");
         System.out.println("Путь к файлу: " + new File(testFilePath).getAbsolutePath());
-        System.out.println("Проверьте файл 'append.txt'.");
-    }
-
-    private static void fillCollectionWithRandomData(BookCollection<Object> collection, int count) {
-        Random random = new Random();
-        String[] sampleData = {"Java", "Kotlin", "Spring", "Maven", "Gradle", "Docker"};
-
-        for (int i = 0; i < count; i++) {
-            String randomElement = sampleData[random.nextInt(sampleData.length)] + " #" + (i + 1);
-            collection.add(randomElement);
-        }
+        System.out.println("Проверьте, что в файле появились записи с заголовком 'Результат случайной генерации'.");
     }
 
     private static BookCollection<Object> createTestCollection() {
