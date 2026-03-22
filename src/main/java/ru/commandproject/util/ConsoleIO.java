b1c9dfb4f21f81159f5f246d8cbe16f754824f0f
ponom <ponom@PONOMNAZ>
2026-03-22 14:16:02 +0300

    Use System.out in ConsoleIO for console-compatible output

--- src/main/java/ru/commandproject/util/ConsoleIO.java
+++ src/main/java/ru/commandproject/util/ConsoleIO.java
@@ -1,15 +1,14 @@
 package ru.commandproject.util;
 
 import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
 
 public final class ConsoleIO {
-    private static final PrintStream UTF8_OUT = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+    private static final PrintStream OUT = System.out;
 
     private ConsoleIO() {
     }
 
     public static PrintStream out() {
-        return UTF8_OUT;
+        return OUT;
     }
 }
